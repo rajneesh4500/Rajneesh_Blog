092748503a2e1b21fc62ba7a4ac02d92af280593
Rajneesh <rajneesh4500@gmail.com>
2020-01-02 03:02:37 +0530

    day 1

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import {
+  About_me,
+  About_mepara,
+  Aboute_mep2,
+  About_mep3,
+  Message_final,
+  Specialities
+} from "../static_content/Aboutme";
 import NavBar from "./NavBar";
-//
-//
-//
-//
-//
-//
 //
 const MainApp = () => {
   return (
     <div>
       <NavBar />
+      <div className="content">
+        <h1>Aboute Me</h1>
+        <p>{About_me}</p>
+        <p>{About_mepara}</p>
+        <p>{Aboute_mep2}</p>
+        <p>{About_mep3}</p>
+        <p>{Message_final}</p>
+      </div>
     </div>
   );
 };
---
+++ src/static_content/Aboutme.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+export const About_me =
+  " I spend sleepless nights coding not because I am bad but because I love coding.";
+
+export const About_mepara =
+  `I beleive in asking only two questions to myself which have  helped me till date to to grow in career` +
+  ` 1. Why is it not working? 2. Why is it working? These two quesitons have always helped` +
+  `grow and keep my curiosity alive.`;
+
+export const Aboute_mep2 =
+  `I never dreamed of loving the work which I do(given I am from Nanotechnology) but ` +
+  `I found my passion in coding. `;
+
+export const About_mep3 = `Currently working as a Full stack developer in Verizon.`;
+
+export const Message_final =
+  "All the projects here are to improve my skills if any issues or suggestions kindly mail to rajneesh4500@gmail.com Feedbacks are always welcome";
+
+export const Specialities = ["Java", "React JS", "Spring Boot", "PL/SQL"];
