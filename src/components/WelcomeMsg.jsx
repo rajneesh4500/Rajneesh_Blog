ef731d88db27f674fd072f15e0e13e6162cd363c
Rajneesh <rajneesh4500@gmail.com>
2020-01-02 01:33:12 +0530

    initial commit2

---
+++ src/components/WelcomeMsg.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//
+//
+//
+//
+//
+//
+//
+const Welcomemsg = () => (
+  <div className="center-msg">
+    <h1>Welcome to my Blog!</h1>
+  </div>
+);
+
+export default Welcomemsg;
